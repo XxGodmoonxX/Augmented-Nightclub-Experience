55a575fc29b81196ae16c2ae8640ff7763669ca6
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2019-01-19 19:18:32 +0900

    Fix

--- Assets/AR-BreakOut/Scripts/GameControllerTutorial.cs
+++ Assets/AR-BreakOut/Scripts/GameControllerTutorial.cs
@@ -25,7 +25,7 @@ public class GameControllerTutorial : PlacenotePunMultiplayerBehaviour {
 		PhotonNetwork.Instantiate("player", Vector3.zero, Quaternion.identity, 0);
 		// PhotonNetwork.Instantiate("shadowPlane", Vector3.zero, Quaternion.identity, 0);
 		// PhotonNetwork.Instantiate("SampleMoon", Vector3.zero, Quaternion.identity, 0);
-		PhotonNetwork.Instantiate("Sphere", Vector3.forward, Quaternion.identity, 0);
+		PhotonNetwork.Instantiate("Sphere", Vector3.zero, Quaternion.identity, 0);
 
 		//画像認識
 		imageanchor.GetComponent<PhotonGenerateImageAnchor>().returnAccess();

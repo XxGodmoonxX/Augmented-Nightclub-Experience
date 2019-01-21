d23b95f2f2f7a059d8960b22dd86b340b1a189a0
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2019-01-21 23:07:10 +0900

    Fix

--- Assets/PlacenoteMultiplayerKit/Examples/SphereController.cs
+++ Assets/PlacenoteMultiplayerKit/Examples/SphereController.cs
@@ -37,16 +37,30 @@ public class SphereController : Photon.MonoBehaviour {
 		// The player should have the same transform as the camera
     // gameObject.transform.position = CameraTransform.position;
     // gameObject.transform.rotation = CameraTransform.rotation;
-    // b.transform.position = CameraTransform.position;
 
-		
 		//タップしたら1回発動
   	if (0 < Input.touchCount) {
+			//タップしたら1回のみ
       if (Input.GetTouch(0).phase == TouchPhase.Began) {
+			//画面触っている間
+      // if (Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(0).phase == TouchPhase.Moved) {
 				// Register this object to the current game controller.
 				// This is important so that all clients have a reference to this object.
 		    breakOutText.GetComponent<BreakOutText>().breakOutInstantiate();
-				// GameController.Instance.RegisterSphere(c);
+
+				// GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+				// Material material = new Material(Shader.Find("Diffuse")) {
+				// 	// color = new Color(Random.value, Random.value, Random.value)
+				// 	color = new Color(0, 0, 200)
+				// };
+				// sphere.GetComponent<Renderer>().material = material;
+				// sphere.transform.position = CameraTransform.transform.TransformPoint(0, 0, 0.5f);
+				// float sphereSize = 1.0f;
+				// sphere.transform.localScale = new Vector3(sphereSize, sphereSize, sphereSize);
+				// gameObject.AddComponent<Rigidbody>();
+				// gameObject.AddComponent<Rigidbody>().AddForce(CameraTransform.TransformDirection(0, 0, 7f), ForceMode.Impulse);
+
+				PhotonNetwork.Instantiate("BlueSphere", Vector3.zero, Quaternion.identity, 0);
 			}
 		}
 	}

72fbfbe060692f7df1e50511a2904b765bc66270
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2019-01-24 15:53:37 +0900

    BLueSphereに別のスクリプトでAddForceできるかな

---
+++ Assets/PlacenoteMultiplayerKit/Examples/SphereAddForce.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereAddForce : MonoBehaviour {
+
+	public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<Rigidbody>().AddForce(cam.transform.TransformDirection(0, 0, 5f),ForceMode.Impulse);
+	}
+}

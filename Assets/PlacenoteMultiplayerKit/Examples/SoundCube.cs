b9ca0baf49d501fe3a1fe8015e09e44f2a3e4074
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2019-02-01 06:11:20 +0900

    左右に波、左右上に球で音表示

--- Assets/PlacenoteMultiplayerKit/Examples/SoundCube.cs
+++ Assets/PlacenoteMultiplayerKit/Examples/SoundCube.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class SoundCube : MonoBehaviour {
 
 	public GameObject soundObject;
-	public GameObject SpherePrefab;
+	public GameObject SpherePrefabRight;
+	public GameObject SpherePrefabLeft;
 
   public Transform CameraTransform;
 
@@ -21,8 +22,15 @@ public class SoundCube : MonoBehaviour {
 		adjustNum = 100; //音量調整用。
 		float volume = soundObject.GetComponent<sound>().volume * adjustNum;
 		float cubeSize = 0.5f;
-		gameObject.transform.position = CameraTransform.TransformPoint(3f, 0, 5f); //カメラの位置に移動して右に1、前に1
-		gameObject.GetComponent<Renderer>().material.color = new Color(0, 0f, 0, 0.5f);
-    gameObject.transform.localScale = new Vector3(cubeSize * volume, cubeSize * volume, cubeSize * volume);
+		// gameObject.transform.position = CameraTransform.TransformPoint(3f, 0, 5f); //カメラの位置に移動して右に1、前に1
+
+		//右上に
+		SpherePrefabRight.transform.position = new Vector3(1f, 3f, 5f);
+		SpherePrefabRight.GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+    SpherePrefabRight.transform.localScale = new Vector3(cubeSize * volume, cubeSize * volume, cubeSize * volume);
+		//左上に
+		SpherePrefabLeft.transform.position = new Vector3(-1f, 3f, 5f); //カメラの位置に移動して右に1、前に1
+		SpherePrefabLeft.GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+    SpherePrefabLeft.transform.localScale = new Vector3(cubeSize * volume, cubeSize * volume, cubeSize * volume);
 	}
 }

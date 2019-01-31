de8d0464b7d9848648eca9ecd9b307eae5671ea7
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2019-02-01 06:11:05 +0900

    右上のボタンでCube真っ白に

---
+++ Assets/PlacenoteMultiplayerKit/Examples/CubeChangeColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeChangeColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void CubeColorChangeWhite() {
+		gameObject.GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+	}
+}
